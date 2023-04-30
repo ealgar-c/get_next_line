9a121bd67592cc198586db0f5f7bbc5ee01b7cf9
Enrique Algar <enriiquealgar@gmail.com>
2023-04-30 14:09:23 +0200

    core proyect

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,66 @@
 /*   By: ealgar-c <ealgar-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 13:41:14 by ealgar-c          #+#    #+#             */
-/*   Updated: 2023/04/28 11:36:02 by ealgar-c         ###   ########.fr       */
+/*   Updated: 2023/04/29 13:57:03 by ealgar-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-/* char	*get_line(int fd)
+char	*get_actual_line(char *acum_str)
 {
-	
+	// esta funcion tiene que recibir el acum_str
+	// y pasar a otro str lo que haya hasta q acabe 
+	// o encuentre un \n y devolver este
 }
-*/
+
+char	*get_stoned(char *acum_str, char *tmp)
+{
+	char	*ret;
+
+	ret = ft_strjoin(acum_str, tmp);
+	free(acum_str);
+	return (ret);
+}
+
+char	*get_buffed(int fd, char *acum_str)
+{
+	char	*tmp;
+	int		i;
+
+	if (acum_str = NULL)
+		acum_str = ft_calloc(1, sizeof(char));
+	tmp = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	i = 1;
+	while (i > 0)
+	{
+		i = read(fd, tmp, BUFFER_SIZE);
+		if (i < 0)
+		{
+			free(tmp);
+			return (NULL);
+		}
+		tmp[i] = '\0';
+		acum_str = get_stoned(acum_str, tmp);
+		if (// aqui queremos una función que busque si ya hay en la temporal un \n)
+			break;
+	}
+	free(tmp);
+	return acum_str;
+}
+
 char	*get_next_line(int fd)
 {
-	static ssize_t	read_chars;
-	char			*buf;
+	static char	*acum_str;
+	char		*act_line;
 
-	buf = malloc(10 * sizeof(char));
-	read_chars += read(fd, buf, 10);
-	return (buf);
+	if (fd < 1)
+		return (NULL);
+	acum_str = get_buffed(fd, acum_str);
+	act_line = get_actual_line(acum_str);
 }
 
 int	main(void)
