7ed96064fa25c6a6c729696d102f158b1d41c5dd
Enrique Algar Ceular <ealgar-c@student.42malaga.com>
2023-05-01 18:12:18 +0200

    reworks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ealgar-c <ealgar-c@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 13:41:14 by ealgar-c          #+#    #+#             */
-/*   Updated: 2023/05/01 16:13:54 by ealgar-c         ###   ########.fr       */
+/*   Updated: 2023/05/01 16:56:37 by ealgar-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ static char	*get_clean_line(char *acum_str)
 {
 	int		i;
 	int		j;
-	char	*r;
+	char	*ret_str;
 
 	i = 0;
 	while (acum_str[i] != '\0' && acum_str[i] != '\n')
@@ -26,23 +26,23 @@ static char	*get_clean_line(char *acum_str)
 		free(acum_str);
 		return (NULL);
 	}
-	r = ft_calloc((ft_strlen(acum_str) - i + 1), sizeof(char));
+	ret_str = ft_calloc((ft_strlen(acum_str) - i + 1), sizeof(char));
 	i++;
 	j = 0;
 	while (acum_str[i] != '\0')
-		r[j++] = acum_str[i++];
+		ret_str[j++] = acum_str[i++];
 	free(acum_str);
-	if (r[0] == '\0')
+	if (ret_str[0] == '\0')
 	{
-		free(r);
+		free(ret_str);
 		return (NULL);
 	}
-	return (r);
+	return (ret_str);
 }
 
 static char	*get_actual_line(char *acum_str)
 {
-	char	*r;
+	char	*ret_str;
 	int		i;
 
 	i = 0;
@@ -50,16 +50,16 @@ static char	*get_actual_line(char *acum_str)
 		return (NULL);
 	while (acum_str[i] != '\0' && acum_str[i] != '\n')
 		i++;
-	r = ft_calloc((i + 2), sizeof(char));
+	ret_str = ft_calloc((i + 2), sizeof(char));
 	i = 0;
 	while (acum_str[i] != '\0' && acum_str[i] != '\n')
 	{
-		r[i] = acum_str[i];
+		ret_str[i] = acum_str[i];
 		i++;
 	}
 	if (acum_str[i] != '\0' && acum_str[i] == '\n')
-		r[i++] = '\n';
-	return (r);
+		ret_str[i++] = '\n';
+	return (ret_str);
 }
 
 static char	*get_joint(char *acum_str, char *buff)
@@ -73,34 +73,34 @@ static char	*get_joint(char *acum_str, char *buff)
 
 static char	*get_a_lot(int fd, char *acum_str)
 {
-	char	*buff;
+	char	*tmp;
 	int		read_char;
 
 	if (!acum_str)
 		acum_str = ft_calloc(1, sizeof(char));
-	buff = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
+	tmp = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
 	read_char = 1;
 	while (read_char > 0)
 	{
-		read_char = read(fd, buff, BUFFER_SIZE);
+		read_char = read(fd, tmp, BUFFER_SIZE);
 		if (read_char < 0)
 		{
-			free(buff);
+			free(tmp);
 			return (NULL);
 		}
-		buff[read_char] = '\0';
-		acum_str = get_joint(acum_str, buff);
-		if (ft_strchr_mod(buff, '\n'))
+		tmp[read_char] = '\0';
+		acum_str = get_joint(acum_str, tmp);
+		if (ft_strchr_mod(tmp, '\n'))
 			break ;
 	}
-	free(buff);
+	free(tmp);
 	return (acum_str);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*acum_str;
-	char		*r;
+	char		*actual_line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -114,7 +114,7 @@ char	*get_next_line(int fd)
 		return (NULL);
 	}
 	acum_str = get_a_lot(fd, acum_str);
-	r = get_actual_line(acum_str);
+	actual_line = get_actual_line(acum_str);
 	acum_str = get_clean_line(acum_str);
-	return (r);
+	return (actual_line);
 }
