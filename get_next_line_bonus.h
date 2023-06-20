4d6a9cb0597625cf0b4b05c74c1c79582dfecc03
Enrique Algar Ceular <ealgar-c@student.42malaga.com>
2023-06-20 19:02:03 +0200

    feat: :sparkles: Added the bonus part

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ealgar-c <ealgar-c@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 13:19:52 by ealgar-c          #+#    #+#             */
+/*   Updated: 2023/06/20 19:01:34 by ealgar-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_calloc(size_t num, size_t size);
+int		ft_strlen(const char *str);
+int		ft_strchr_mod(char *s, int c);
+
+#endif
